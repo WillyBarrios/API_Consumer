3c49ee162453a424ae2667aaf3b2b798509b437e
¨WillyBarrios¨ <romeob-@outlook.com>
2025-07-24 11:52:07 -0600

    Realice los cambios en el Main y añadi la clase auditor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.anotation.JsonProperty;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
+import com.fasterxml.jackson.databind.JsonNode;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static class RaizProceso{
+        @JsonProperty("id")
+        public String id;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        @JsonProperty ("sistemaOrigen")
+        public String sistemaOrigen;
+
+        @JsonProperty("fechaGeneracion")
+        public String fechaGeneracion;
+
+        @JsonProperty("estadoGeneral")
+        public String estadoGeneral;
+
+        @JsonProperty("procesos")
+        public List<Proceso> procesos;
+
+        @JsonProperty("auditor")
+        public Auditor auditor;
+
+        @JsonProperty("metadata")
+        public Metadata metadata;
+    }
+    public class auditor {
+        @JsonProperty("nombre")
+        public String nombre;
+
+        @JsonProperty("departamento")
+        public String departamento;
     }
 }
